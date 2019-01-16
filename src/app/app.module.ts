5eac9978cf9331da4005d9a3d13ce1073f20b714
Michael Vietluzhskykh <mishavetl@gmail.com>
2019-01-16 17:32:19 +0200

    make draggables find closest text spots and move to ngx-codemirror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {CodemirrorModule} from 'ng2-codemirror';
+import {CodemirrorModule} from '@ctrl/ngx-codemirror';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/draggable/draggable.component.ts
+++ src/app/draggable/draggable.component.ts
@@ -22,24 +22,52 @@ export class DraggableComponent implements OnInit {
     };
   }
 
+  wrapTextWithSpans() {
+    document.querySelectorAll('app-rules-constructor .CodeMirror-line > span').forEach(lineElement => {
+      for (let i = 0; i < lineElement.childNodes.length; ++i) {
+        if (lineElement.childNodes[i].nodeType === Node.TEXT_NODE) {
+          const textValue = lineElement.childNodes[i].nodeValue;
+          const symbols = textValue.split(' ');
+          lineElement.childNodes[i].remove();
+          for (let j = symbols.length - 1; j >= 0; --j) {
+            const textSpan = document.createElement('span');
+            textSpan.innerHTML = symbols[j];
+            if (j === symbols.length - 1 && symbols[j] === '') {
+              continue;
+            }
+            if (j < symbols.length - 1) {
+              textSpan.innerHTML += ' ';
+            }
+            lineElement.insertBefore(textSpan, lineElement.childNodes.item(i));
+          }
+        }
+      }
+    });
+  }
+
   checkPlaces(elementCoords) {
     let closestElement = null;
     let minDistance = Number.MAX_VALUE;
     // console.log(document.querySelectorAll(':hover'));
     // console.log(document.elementsFromPoint(elementCoords.x, elementCoords.y));
     document.querySelectorAll('app-rules-constructor .CodeMirror-line > span > span').forEach(placeElement => {
-      const coords = placeElement.getBoundingClientRect();
+      const coords = <DOMRect>placeElement.getBoundingClientRect();
       const distance = Math.hypot(coords.x - Number(elementCoords.x), coords.y - Number(elementCoords.y));
       // const distance = Math.hypot(coords.x - Number(elementCoords.x), coords.y - Number(elementCoords.y));
 
+      placeElement.classList.remove('draggable-constructor-selected-before');
+      placeElement.classList.remove('draggable-constructor-selected-after');
       if (minDistance > distance) {
         minDistance = distance;
         closestElement = placeElement;
       }
-      if (minDistance > distance) {
-
-      }
     });
+    if (closestElement !== null) {
+      if (closestElement.previousSibling !== null) {
+        closestElement.previousSibling.classList.add('draggable-constructor-selected-after');
+      }
+      closestElement.classList.add('draggable-constructor-selected-before');
+    }
     return {element: closestElement, distance: minDistance};
   }
 
@@ -48,25 +76,26 @@ export class DraggableComponent implements OnInit {
   }
 
   onMoved(event) {
-    // console.log('moved: ', event);
     const centerCoords = this.getCenterCoords(event.source._rootElement);
-    // console.log([centerCoords.x, centerCoords.y]);
     const found = this.checkPlaces(centerCoords);
     if (found.distance < 20) {
       console.log(found.element);
     }
-    // // console.log(document.elementFromPoint(centerCoords.x + event.x, centerCoords.y + event.y));
   }
 
   onStarted(event) {
+    this.wrapTextWithSpans();
     // console.log('started: ', event);
   }
 
   onEnded(event) {
-    // console.log('ended: ', event);
     event.source.element.nativeElement.style.transform = 'none'; // visually reset element to its origin
     const source: any = event.source;
     source._passiveTransform = { x: 0, y: 0 }; // make it so new drag starts from same origin
+    const codeMirror = document.querySelector('#configurationCodeEditor .CodeMirror').CodeMirror;
+    codeMirror.setValue(codeMirror.getValue());
+    codeMirror.getDoc().undo();
+    console.log();
   }
 
 }
